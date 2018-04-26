31765e74c1bda0795e3a844afface84ca46bf3f9
Alejandro Colli <32985001+ancolli@users.noreply.github.com>
2018-04-26 14:49:42 -0300

    initial commit

---
+++ applications/utilities/BC/regionCoupleSolidFluid/regionCoupleSolidFvPatchScalarField.C
@@ -0,0 +1,267 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     |
+    \\  /    A nd           | Copyright (C) 2018 OpenFOAM Foundation
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+\*---------------------------------------------------------------------------*/
+
+#include "regionCoupleSolidFvPatchScalarField.H"
+#include "regionCoupleFluidFvPatchScalarField.H"
+#include "addToRunTimeSelectionTable.H"
+#include "fvPatchFieldMapper.H"
+#include "volFields.H"
+#include "mappedPatchBase.H"
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+namespace Foam
+{
+
+// * * * * * * * * * * * * * * * * Constructors  * * * * * * * * * * * * * * //
+
+regionCoupleSolidFvPatchScalarField::
+regionCoupleSolidFvPatchScalarField
+(
+    const fvPatch& p,
+    const DimensionedField<scalar, volMesh>& iF
+)
+:
+    mixedFvPatchScalarField(p, iF),
+    tCoupledBase(patch(), "undefined", "undefined", "undefined-K"),
+    TnbrName_("undefined-Tnbr"),
+    exchangeCurrentDensity_(0),
+    beta_(0),
+    equilibriumPotential_(0)
+{
+    this->refValue() = 0.0;
+    this->refGrad() = 0.0;
+    this->valueFraction() = 1.0;
+}
+
+
+regionCoupleSolidFvPatchScalarField::
+regionCoupleSolidFvPatchScalarField
+(
+    const regionCoupleSolidFvPatchScalarField& ptf,
+    const fvPatch& p,
+    const DimensionedField<scalar, volMesh>& iF,
+    const fvPatchFieldMapper& mapper
+)
+:
+    mixedFvPatchScalarField(ptf, p, iF, mapper),
+    tCoupledBase(patch(), ptf),
+    TnbrName_(ptf.TnbrName_),
+    exchangeCurrentDensity_(ptf.exchangeCurrentDensity_),
+    beta_(ptf.beta_),
+    equilibriumPotential_(ptf.equilibriumPotential_)
+
+{}
+
+
+regionCoupleSolidFvPatchScalarField::
+regionCoupleSolidFvPatchScalarField
+(
+    const fvPatch& p,
+    const DimensionedField<scalar, volMesh>& iF,
+    const dictionary& dict
+)
+:
+    mixedFvPatchScalarField(p, iF),
+    tCoupledBase(patch(), dict),
+    TnbrName_(dict.lookup("Tnbr")),
+    exchangeCurrentDensity_(0),
+    beta_(0),
+    equilibriumPotential_(0)
+
+{
+    if (!isA<mappedPatchBase>(this->patch().patch()))
+    {
+        FatalErrorInFunction
+            << "' not type '" << mappedPatchBase::typeName << "'"
+            << "\n    for patch " << p.name()
+            << " of field " << internalField().name()
+            << " in file " << internalField().objectPath()
+            << exit(FatalError);
+    }
+
+    // search for kinetic parameters
+    {
+        dict.lookup("j0") >> exchangeCurrentDensity_;
+        dict.lookup("b") >> beta_;
+	dict.lookup("E0") >> equilibriumPotential_;
+    }
+
+    fvPatchScalarField::operator=(scalarField("value", dict, p.size()));
+
+    if (dict.found("refValue"))
+    {
+        // Full restart
+        refValue() = scalarField("refValue", dict, p.size());
+        refGrad() = scalarField("refGradient", dict, p.size());
+        valueFraction() = scalarField("valueFraction", dict, p.size());
+    }
+    else
+    {
+        // Start from user entered data. Assume fixedValue.
+        refValue() = *this;
+        refGrad() = 0.0;
+        valueFraction() = 1.0;
+    }
+}
+
+
+regionCoupleSolidFvPatchScalarField::
+regionCoupleSolidFvPatchScalarField
+(
+    const regionCoupleSolidFvPatchScalarField& wtcsf,
+    const DimensionedField<scalar, volMesh>& iF
+)
+:
+    mixedFvPatchScalarField(wtcsf, iF),
+    tCoupledBase(patch(), wtcsf),
+    TnbrName_(wtcsf.TnbrName_),
+    exchangeCurrentDensity_(wtcsf.exchangeCurrentDensity_),
+    beta_(wtcsf.beta_),
+    equilibriumPotential_(wtcsf.equilibriumPotential_)
+{}
+
+
+// * * * * * * * * * * * * * * * Member Functions  * * * * * * * * * * * * * //
+
+void regionCoupleSolidFvPatchScalarField::updateCoeffs()
+{
+    if (updated())
+    {
+        return;
+    }
+
+    // Since we're inside initEvaluate/evaluate there might be processor
+    // comms underway. Change the tag we use.
+    int oldTag = UPstream::msgType();
+    UPstream::msgType() = oldTag+1;
+
+    // Get the coupling information from the mappedPatchBase
+    const mappedPatchBase& mpp =
+        refCast<const mappedPatchBase>(patch().patch());
+    const polyMesh& nbrMesh = mpp.sampleMesh();
+    const label samplePatchi = mpp.samplePolyPatch().index();
+    const fvPatch& nbrPatch =
+        refCast<const fvMesh>(nbrMesh).boundary()[samplePatchi];
+
+    // Calculate the temperature by harmonic averaging
+    // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    const regionCoupleFluidFvPatchScalarField& nbrField =
+    refCast
+    <
+        const regionCoupleFluidFvPatchScalarField
+    >
+    (
+        nbrPatch.lookupPatchField<volScalarField, scalar>
+        (
+            TnbrName_
+        )
+    );
+
+// Swap to obtain full local values of neighbour internal field
+    tmp<scalarField> nbrPotencial(new scalarField(nbrField.size(), 0.0));
+
+    nbrPotencial.ref() = nbrField;
+
+    tmp<scalarField> myKDelta = kappa(*this)*patch().deltaCoeffs();
+    tmp<scalarField> potential = patchInternalField() + snGrad()/patch().deltaCoeffs();
+		
+    mpp.distribute(nbrPotencial.ref());
+
+    tmp<scalarField> Ai = 0*nbrPotencial();
+    tmp<scalarField> Bi = 0*nbrPotencial();
+
+// For more than one reaction at the same electrode
+    if (exchangeCurrentDensity_.size() > 0)
+    {
+    	forAll(exchangeCurrentDensity_, iReaction)
+        {
+        	Ai = Ai() + exchangeCurrentDensity_[iReaction]*exp((potential()-nbrPotencial()-equilibriumPotential_[iReaction])/beta_[iReaction])/beta_[iReaction]/myKDelta();
+		Bi = Bi() + exchangeCurrentDensity_[iReaction]*exp((potential()-nbrPotencial()-equilibriumPotential_[iReaction])/beta_[iReaction])/myKDelta();
+        }
+    }
+
+// Explanation in the paper
+
+    this->refValue() = potential()-Bi()/Ai();
+    this->refGrad() = 0;
+    this->valueFraction() = Ai()/(1+Ai());
+
+    mixedFvPatchScalarField::updateCoeffs();
+
+    if (debug)
+    {
+        scalar Q = gSum(kappa(*this)*patch().magSf()*snGrad());
+
+        Info<< patch().boundaryMesh().mesh().name() << ':'
+            << patch().name() << ':'
+            << this->internalField().name() << " <- "
+            << nbrMesh.name() << ':'
+            << nbrPatch.name() << ':'
+            << this->internalField().name() << " :"
+            << " current:" << Q
+            << " wallPotential "
+            << " min:" << gMin(*this)
+            << " max:" << gMax(*this)
+            << " avg:" << gAverage(*this)
+            << endl;
+    }
+
+    // Restore tag
+    UPstream::msgType() = oldTag;
+}
+
+
+void regionCoupleSolidFvPatchScalarField::write
+(
+    Ostream& os
+) const
+{
+    mixedFvPatchScalarField::write(os);
+    os.writeKeyword("Tnbr")<< TnbrName_
+        << token::END_STATEMENT << nl;
+    exchangeCurrentDensity_.writeEntry("j0", os);
+    beta_.writeEntry("b", os);
+    equilibriumPotential_.writeEntry("E0", os);
+    tCoupledBase::write(os);
+}
+
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+makePatchTypeField
+(
+    fvPatchScalarField,
+    regionCoupleSolidFvPatchScalarField
+);
+
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+} // End namespace Foam
+
+
+// ************************************************************************* //
